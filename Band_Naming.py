61fe4442c77710e4c1a4d8e2b00fce31761c3054
Omm Devgoswami <teamuniverse.omm@gmail.com>
2024-06-24 18:06:17 +0530

    first commit

---
+++ Band_Naming.py
@@ -0,0 +1,4 @@
+print ("Hello !! Welcome to the Band-Name-Generator")
+city = input("Enter your City's name: \n")
+pet = input("Enter your Pet's name: \n")
+print("Your new Band-Name is : "+city+"-"+pet)
---
+++ Hello_World.py
@@ -0,0 +1,7 @@
+print ("Hello World")
+print ("This is my First Python Program under this Course.")
+print() #Empty Line
+# print ("She said "Hello" and then left.")
+    #Error Line 
+print ('She said "Hello" and then left.')
+print ("She said \"Hello \" and then left") #\" .... \" : Escape line backslash 

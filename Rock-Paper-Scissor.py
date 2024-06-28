b7eac5f81476e92847be26a4b7d1b4cd12b8a517
OmmDevgoswami <teamuniverse.omm@gmail.com>
2024-06-28 17:23:31 +0530

    Added ASCII Arts

--- Rock-Paper-Scissor.py
+++ Rock-Paper-Scissor.py
@@ -30,37 +30,37 @@ print ("Wlcome to the Rock-Paper-Scissor Python Game !!")
 player = int (input ("Make a Choice:- 1.Rock, 2.Paper , 3.Scissor\n"))
 computer = random.randint(1,3)
 if (player == 1):
-    print ("Player Chose: Rock")
+    print ("Player Chose: Rock"+rock)
     if (computer == 1):
-        print ("Computer Chose: Rock")
+        print ("Computer Chose: Rock"+rock)
         print ("This is a Tie !!")
     elif (computer == 2):
-        print ("Computer Chose: Paper")
+        print ("Computer Chose: Paper"+paper)
         print ("You Lose !!")
     else:
-        print ("Computer Chose: Scissor")
+        print ("Computer Chose: Scissor"+scissors)
         print ("You Win !!")
 elif (player == 2):
-    print ("Player Chose: Paper")
+    print ("Player Chose: Paper"+paper)
     if (computer == 2):
-        print ("Computer Chose: Paper")
+        print ("Computer Chose: Paper"+paper)
         print ("This is a Tie !!")
     elif (computer == 3):
-        print ("Computer Chose: Scissor")
+        print ("Computer Chose: Scissor"+scissors)
         print ("You Lose !!")
     else:
-        print ("Computer Chose: Rock")
+        print ("Computer Chose: Rock"+rock)
         print ("You Win !!")
 elif (player == 3):
-    print ("Player Chose: Scissor")
+    print ("Player Chose: Scissor"+scissors)
     if (computer == 3):
-        print ("Computer Chose: Scissor")
+        print ("Computer Chose: Scissor"+scissors)
         print ("This is a Tie !!")
     elif (computer == 1):
-        print ("Computer Chose: Rock")
+        print ("Computer Chose: Rock"+rock)
         print ("You Lose !!")
     else:
-        print ("Computer Chose: Paper")
+        print ("Computer Chose: Paper"+paper)
         print ("You Win !!")
 else:
     print ("Invlaid Choice Entered !!")

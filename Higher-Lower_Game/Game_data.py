190cffc0563492109f00a194bd5edffaf049e55a
OmmDevgoswami <teamuniverse.omm@gmail.com>
2024-07-24 15:09:44 +0530

    Higher-Lower Game Commited !!

---
+++ Higher-Lower_Game/Game_data.py
@@ -0,0 +1,254 @@
+data = [
+    {
+        'name': 'Cristiano Ronaldo',
+        'profession': 'Footballer',
+        'country': 'Portugal',
+        'follower_count': 344000000,
+        'ign': 'cristiano'
+    },
+    {
+        'name': 'Dwayne Johnson',
+        'profession': 'Actor',
+        'country': 'United States',
+        'follower_count': 276000000,
+        'ign': 'therock'
+    },
+    {
+        'name': 'Selena Gomez',
+        'profession': 'Singer',
+        'country': 'United States',
+        'follower_count': 283000000,
+        'ign': 'selenagomez'
+    },
+    {
+        'name': 'Kylie Jenner',
+        'profession': 'Businesswoman',
+        'country': 'United States',
+        'follower_count': 294000000,
+        'ign': 'kyliejenner'
+    },
+    {
+        'name': 'Lionel Messi',
+        'profession': 'Footballer',
+        'country': 'Argentina',
+        'follower_count': 262000000,
+        'ign': 'leomessi'
+    },
+    {
+        'name': 'Kim Kardashian',
+        'profession': 'Media Personality',
+        'country': 'United States',
+        'follower_count': 256000000,
+        'ign': 'kimkardashian'
+    },
+    {
+        'name': 'Beyoncé',
+        'profession': 'Singer',
+        'country': 'United States',
+        'follower_count': 199000000,
+        'ign': 'beyonce'
+    },
+    {
+        'name': 'Virat Kohli',
+        'profession': 'Cricketer',
+        'country': 'India',
+        'follower_count': 180000000,
+        'ign': 'virat.kohli'
+    },
+    {
+        'name': 'Deepika Padukone',
+        'profession': 'Actor',
+        'country': 'India',
+        'follower_count': 70000000,
+        'ign': 'deepikapadukone'
+    },
+    {
+        'name': 'Sushant Singh Rajput',
+        'profession': 'Actor',
+        'country': 'India',
+        'follower_count': 17000000,
+        'ign': 'sushantsinghrajput'
+    },
+    {
+        'name': 'Shakira',
+        'profession': 'Singer',
+        'country': 'Colombia',
+        'follower_count': 80000000,
+        'ign': 'shakira'
+    },
+    {
+        'name': 'Priyanka Chopra',
+        'profession': 'Actor',
+        'country': 'India',
+        'follower_count': 68000000,
+        'ign': 'priyankachopra'
+    },
+    {
+        'name': 'Jennifer Lopez',
+        'profession': 'Singer',
+        'country': 'United States',
+        'follower_count': 168000000,
+        'ign': 'jlo'
+    },
+    {
+        'name': 'Neymar Jr',
+        'profession': 'Footballer',
+        'country': 'Brazil',
+        'follower_count': 167000000,
+        'ign': 'neymarjr'
+    },
+    {
+        'name': 'Kevin Hart',
+        'profession': 'Actor',
+        'country': 'United States',
+        'follower_count': 118000000,
+        'ign': 'kevinhart4real'
+    },
+    {
+        'name': 'Ariana Grande',
+        'profession': 'Singer',
+        'country': 'United States',
+        'follower_count': 255000000,
+        'ign': 'arianagrande'
+    },
+    {
+        'name': 'Shah Rukh Khan',
+        'profession': 'Actor',
+        'country': 'India',
+        'follower_count': 50000000,
+        'ign': 'iamsrk'
+    },
+    {
+        'name': 'Lady Gaga',
+        'profession': 'Singer',
+        'country': 'United States',
+        'follower_count': 98000000,
+        'ign': 'ladygaga'
+    },
+    {
+        'name': 'Bad Bunny',
+        'profession': 'Singer',
+        'country': 'Puerto Rico',
+        'follower_count': 112000000,
+        'ign': 'badbunnypr'
+    },
+    {
+        'name': 'Tom Holland',
+        'profession': 'Actor',
+        'country': 'United Kingdom',
+        'follower_count': 50000000,
+        'ign': 'tomholland2013'
+    },
+    {
+        'name': 'Lalisa Manoban',
+        'profession': 'Singer',
+        'country': 'Thailand',
+        'follower_count': 70000000,
+        'ign': 'lalalalisa_m'
+    },
+    {
+        'name': 'Maluma',
+        'profession': 'Singer',
+        'country': 'Colombia',
+        'follower_count': 70000000,
+        'ign': 'maluma'
+    },
+    {
+        'name': 'Hugh Jackman',
+        'profession': 'Actor',
+        'country': 'Australia',
+        'follower_count': 30000000,
+        'ign': 'thehughjackman'
+    },
+    {
+        'name': 'Kriti Sanon',
+        'profession': 'Actor',
+        'country': 'India',
+        'follower_count': 30000000,
+        'ign': 'kritisanon'
+    },
+    {
+        'name': 'Ricky Martin',
+        'profession': 'Singer',
+        'country': 'Puerto Rico',
+        'follower_count': 25000000,
+        'ign': 'ricky_martin'
+    },
+    {
+        'name': 'Gal Gadot',
+        'profession': 'Actor',
+        'country': 'Israel',
+        'follower_count': 50000000,
+        'ign': 'gal_gadot'
+    },
+    {
+        'name': 'Akshay Kumar',
+        'profession': 'Actor',
+        'country': 'India',
+        'follower_count': 45000000,
+        'ign': 'akshaykumar'
+    },
+    {
+        'name': 'Drake',
+        'profession': 'Rapper',
+        'country': 'Canada',
+        'follower_count': 90000000,
+        'ign': 'champagnepapi'
+    },
+    {
+        'name': 'Billie Eilish',
+        'profession': 'Singer',
+        'country': 'United States',
+        'follower_count': 87000000,
+        'ign': 'billieeilish'
+    },
+    {
+        'name': 'Zendaya',
+        'profession': 'Actor',
+        'country': 'United States',
+        'follower_count': 113000000,
+        'ign': 'zendaya'
+    },
+    {
+        'name': 'Chris Brown',
+        'profession': 'Singer',
+        'country': 'United States',
+        'follower_count': 89000000,
+        'ign': 'chrisbrownofficial'
+    },
+    {
+        'name': 'Chris Hemsworth',
+        'profession': 'Actor',
+        'country': 'Australia',
+        'follower_count': 65000000,
+        'ign': 'chrishemsworth'
+    },
+    {
+        'name': 'James Rodríguez',
+        'profession': 'Footballer',
+        'country': 'Colombia',
+        'follower_count': 50000000,
+        'ign': 'jamesrodriguez10'
+    },
+    {
+        'name': 'Lele Pons',
+        'profession': 'Internet Personality',
+        'country': 'United States',
+        'follower_count': 49000000,
+        'ign': 'lelepons'
+    },
+    {
+        'name': 'Rick Ross',
+        'profession': 'Rapper',
+        'country': 'United States',
+        'follower_count': 10000000,
+        'ign': 'richforever'
+    },
+    {
+        'name': 'Karol G',
+        'profession': 'Singer',
+        'country': 'Colombia',
+        'follower_count': 55000000,
+        'ign': 'karolg'
+    },
+]

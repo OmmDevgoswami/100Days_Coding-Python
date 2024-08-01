14d5ba38a63e7d155847eaf30a7e22e3eff3799d
OmmDevgoswami <teamuniverse.omm@gmail.com>
2024-08-01 23:24:27 +0530

    Etch-A-Sketch Game added !!

---
+++ Turtle_Into/Etch-A-Sketch_Turtle.py
@@ -0,0 +1,36 @@
+import turtle
+
+turt = turtle.Turtle()
+screen = turtle.Screen()
+screen.title("Etch-A-Sketch")
+turt.speed("fastest")
+
+def moveForward():
+    turt.forward(10)
+    
+def moveBackward():
+    turt.backward(10)
+    
+def rotateLeft():
+    turt.left(10)
+    
+def rotateRight():
+    turt.right(10)
+    
+def clearScreen():
+    # turt.speed("fastest")
+    # turt.home()
+    # turt.clear()
+    # turt.speed("normal")
+    turt.reset()
+    
+    
+screen.onkey(fun = moveForward, key = "w")
+screen.onkey(fun = moveBackward, key = "s")
+screen.onkey(fun = rotateLeft, key = "a")
+screen.onkey(fun = rotateRight, key = "d")
+
+screen.onkey(fun = clearScreen, key = "c")
+screen.listen()        
+
+turtle.done()

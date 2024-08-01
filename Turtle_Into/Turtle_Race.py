bc8c843443d025f910fefbe7b8bd31b92d8edc0f
OmmDevgoswami <teamuniverse.omm@gmail.com>
2024-08-02 00:17:44 +0530

    Finishing Line Added !!

--- Turtle_Into/Turtle_Race.py
+++ Turtle_Into/Turtle_Race.py
@@ -3,9 +3,25 @@ import turtle, random
 screen = turtle.Screen()
 screen.title("The Turtle Race is Here !!!!")
 screen.setup(width = 1000, height = 600)
+screen.bgcolor("black")
 colors = ["Red", "Orange", "Yellow", "Green", "Blue", "Indigo", "Violet"]
 racers = {}
-user_bet = turtle.textinput("Make a Bet", "Which Rainbow Bow are you Manifesting On !!!")
+user_bet = turtle.textinput("Make a Bet", "Which Rainbow Bow are you Manifesting On !!!").capitalize()
+turt = turtle.Turtle()
+turt.hideturtle()
+
+def finishing_line():
+    turt.penup()
+    turt.goto(430,-130)
+    turt.pendown()
+    turt.left(90)
+    turt.color("white")
+    turt.forward(280)
+    turt.penup()
+    turt.home()
+    turt.pendown()
+    
+finishing_line()   
 
 y = -120
 for _ in colors:
@@ -21,6 +37,7 @@ while flag:
         if racers[color].xcor() > 430:
             flag = False
             winner = racers[color].pencolor()
+            col = color
             if user_bet == winner:
                 print(f"Congratulations, you won! The winner is {winner}!")
             else:
@@ -28,6 +45,11 @@ while flag:
         else:
             random_speed = random.randint(0,10)
             racers[color].forward(random_speed)
+
+
+screen.title(f"The winner is {winner}!")            
+turt.color(col)
+turt.write(f"The winner is {winner}!",align='center', font=('Arial', 20, 'normal'))
 
 screen.exitonclick()
 

f724740aae1e1f3a03eebd057f65e85ce4b3b98a
OmmDevgoswami <teamuniverse.omm@gmail.com>
2024-08-16 23:13:27 +0530

    Map Game Added !!

---
+++ Guess_The_State/main.py
@@ -0,0 +1,30 @@
+import turtle
+import pandas
+
+screen = turtle.Screen()
+screen.title("Guess The Indian State")
+screen.setup(width = 650, height = 700)
+screen.addshape("Guess_The_State\\Indian_Map.gif")
+turtle.shape("Guess_The_State\\Indian_Map.gif")
+
+location = pandas.read_csv("Guess_The_State\\Indian_states.csv")
+all_states = location["State"].to_list()
+
+guess = screen.textinput("Guess The State","Enter The Name of the Next State ?").title()
+score = 0
+
+flag = True
+while flag:
+    if guess in all_states:
+        state_name = turtle.Turtle()
+        state_name.hideturtle()
+        state_name.penup()
+        new_location = location[location.State == guess]
+        score += 1
+        state_name.goto(x = new_location["Latitude"].item(), y = new_location["Longitude"].item())
+        state_name.write(new_location["State"].item())
+    guess = screen.textinput(f"{score}/29 Guessed Correctly","Enter The Name of the Next State ?").title()
+    if score > 28:
+        flag = False
+
+turtle.done()

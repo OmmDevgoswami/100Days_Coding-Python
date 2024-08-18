1ea7c22d3a9fd621d99434dc889695a248d15c40
OmmDevgoswami <teamuniverse.omm@gmail.com>
2024-08-18 23:26:42 +0530

    comprehension

--- List-Dict_Comprehension/list_comprehension.py
+++ List-Dict_Comprehension/list_comprehension.py
@@ -1,11 +1,11 @@
 value1_list = []
 value2_list = []
-with open ("List_Comphresion\\file1.txt" , "r") as file:
+with open ("List-Dict_Comprehension\\file1.txt" , "r") as file:
     value1 = file.readlines()
     value1_list = [int(num.strip()) for num in value1]
     value1_list.sort()
     
-with open ("List_Comphresion\\file2.txt" , "r") as file:
+with open ("List-Dict_Comprehension\\file2.txt" , "r") as file:
     value2 = file.readlines()
     value2_list = [int(num.strip()) for num in value2]
     value2_list.sort()

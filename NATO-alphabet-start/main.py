827602d8996aa24f3293f94fa38ea9cb7fb0d76a
OmmDevgoswami <teamuniverse.omm@gmail.com>
2024-08-19 07:07:26 +0530

    Deleted unused code.

--- NATO-alphabet-start/main.py
+++ NATO-alphabet-start/main.py
@@ -1,16 +1,3 @@
-
-# for (index, row) in student_data_frame.iterrows():
-#     #Access index and row
-#     #Access row.student or row.score
-#     pass
-
-# # Keyword Method with iterrows()
-# # {new_key:new_value for (index, row) in df.iterrows()}
-
-# #TODO 1. Create a dictionary in this format:
-# {"A": "Alfa", "B": "Bravo"}
-
-# #TODO 2. Create a list of the phonetic code words from a word that the user inputs.
 import pandas
 data = pandas.read_csv("NATO-alphabet-start\\nato_phonetic_alphabet.csv")
 nato_phonetic = {row.letter:row.code for (index,row) in data.iterrows()}

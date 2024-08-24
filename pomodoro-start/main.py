dd896a755a60907e1577d8c5c574c01c2ad16e1f
OmmDevgoswami <teamuniverse.omm@gmail.com>
2024-08-24 19:44:02 +0530

    Pomodoro Timer ready

--- pomodoro-start/main.py
+++ pomodoro-start/main.py
@@ -1,4 +1,5 @@
 from tkinter import *
+from playsound import playsound
 import math
 
 # ---------------------------- CONSTANTS ------------------------------- #
@@ -19,17 +20,33 @@ screen.minsize(width = 500, height = 500)
 screen.config(padx = 120, pady = 120, bg = YELLOW)
 screen.title("Pomodoro")
 
+# ---------------------------- WINDOW FOCUS ------------------------------- # 
+def attention():
+    screen.deiconify()
+    screen.attributes('-topmost', True)  
+    screen.focus_force()
+    
+
 # ---------------------------- TIMER RESET ------------------------------- # 
 timer = Label(text = "TIMER",width = 11,bg = YELLOW, fg = GREEN, font = (FONT_NAME, 30, "bold"))
 timer.grid(row = 0, column = 1)
+
+def reset():
+    screen.after_cancel(activity)
+    global rep
+    rep = 0
+    timer.config(text = "TIMER", fg = GREEN)
+    canvas.itemconfig(timer_record, text = "00:00")
+    tick.config(text = "")
+    start["state"] = "normal"
 
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
 def start_time():
     global rep 
     rep += 1
-    work_time = WORK_MIN 
-    short_break = SHORT_BREAK_MIN 
-    long_break = LONG_BREAK_MIN 
+    work_time = WORK_MIN * 60
+    short_break = SHORT_BREAK_MIN * 60
+    long_break = LONG_BREAK_MIN * 60
     
     if rep%8 == 0:
         count_down(long_break)
@@ -63,6 +80,13 @@ def count_down(count):
         activity = screen.after(1000, count_down, count - 1)
     else:
         start_time()
+        tick_marks  = ""
+        for _ in range(math.floor(rep/2)):
+            tick_marks += "✔"
+        tick.config(text = tick_marks)
+        attention()
+        playsound("pomodoro-start\\NOTIFIKASI WA KEREN 2.mp3")
+        
 # ---------------------------- UI SETUP ------------------------------- #
 canvas = Canvas(width = 200, height = 224, bg = YELLOW, highlightthickness = 0)
 tomato = PhotoImage(file = "pomodoro-start\\tomato.gif")
@@ -70,12 +94,12 @@ canvas.create_image(100, 112, image = tomato)
 timer_record = canvas.create_text(100, 130, text = "00:00", font =(FONT_NAME, 35, "bold"),fill = "#FFFFFF")
 canvas.grid(row = 1, column = 1)
  
-start = Button(text = "Start", highlightthickness = 0, command = start_time)
-reset = Button(text = "Reset", highlightthickness = 0)
+start = Button(text = "Start", highlightthickness = 0,fg = PINK, bg = YELLOW, font = (FONT_NAME, 12, "bold"),bd = 0, command = start_time)
+reset = Button(text = "Reset", highlightthickness = 0,fg = RED, bg = YELLOW, font = (FONT_NAME, 12, "bold"),bd = 0, command = reset)
 start.grid(row = 2, column = 0)
 reset.grid(row = 2, column = 2)
 
-tick = Label(text = "✔",bg = YELLOW, fg = GREEN, font = (20))
+tick = Label(bg = YELLOW, fg = GREEN, font = (20))
 tick.grid(row = 3, column = 1)
 
 screen.mainloop()

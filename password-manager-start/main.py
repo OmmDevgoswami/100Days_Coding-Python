9333cb11a1246d9bafe78c1b43ed0a7ba559671c
OmmDevgoswami <teamuniverse.omm@gmail.com>
2024-09-15 14:57:04 +0530

    Value Storing Successfully

--- password-manager-start/main.py
+++ password-manager-start/main.py
@@ -1,5 +1,6 @@
 from tkinter import *
 from tkinter import font
+import pandas
 
 PINK = "#e2979c"
 RED = "#e7305b"
@@ -17,10 +18,36 @@ font_path_1 = "password-manager-start\\Font\\Fjalla_One\\FjallaOne-Regular.ttf"
 FONT_1 = font.Font(family="Fjalla One", size=14)
 font_path_2 = "password-manager-start\\Font\\Hammersmith_One\\HammersmithOne-Regular.ttf"
 FONT_2 = font.Font(family="Hammersmith One", size=14)
+# ---------------------------- SAVER-SCREEN SETUP ------------------------------- #
+
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
-
+def save():
+    website = website_entry.get()
+    email = email_entry.get()
+    user = user_entry.get()
+    password = password_entry.get()
+    
+    with open("password-manager-start\\Details.txt", 'a') as file:
+        file.write(f"\n{website} | {email} | {user} | {password}")
+    
+    Data = {
+            "Website":[website],
+            "Email":[email],
+            "User":[user],
+            "Password":[password]
+        }
+    new_data = pandas.DataFrame(Data)
+    new_data.to_csv("password-manager-start\\Deatils.csv",mode='a', header=False)
+    
+    website_entry.delete(0, END)
+    email_entry.delete(0, END)
+    user_entry.delete(0,END)
+    password_entry.delete(0,END)
+    print("Value Recorded Successfully!!!")
+    website_entry.focus()
+    
 # ---------------------------- UI SETUP ------------------------------- #
 canvas =  Canvas(width=300, height=200, highlightthickness=0, bg= YELLOW)
 icon = PhotoImage(file="password-manager-start\\logo.png")
@@ -30,6 +57,7 @@ canvas.grid(row=1, column=1)
 website_label = Label(text="Website: ",font = FONT_1,bg = YELLOW, fg = PINK)
 website_label.grid(row=2, column=0, sticky = "e")
 website_entry = Entry(width = 35, font = FONT_2, bg = GRAY, fg = VIOLET)
+website_entry.focus()
 website_entry.grid(row=2, column=1, columnspan=2, sticky = "w")
 
 email_label = Label(text="Email: ",font = FONT_1, bg = YELLOW, fg = PINK)
@@ -49,7 +77,7 @@ password_entry.grid(row=5, column=1,columnspan=2, sticky= "w")
 pass_generator = Button(text = "Generate", font = FONT_2, highlightthickness = 0,bd = 0, bg = YELLOW, fg = RED,width = 10, relief=RAISED)
 pass_generator.grid(row = 5, column = 2)
 
-add_button = Button(text = "Record", font = FONT_2, highlightthickness = 0,bd = 0, bg = YELLOW, fg = RED, width = 40, relief=RAISED)
+add_button = Button(text = "Record", font = FONT_2, highlightthickness = 0,bd = 0, bg = YELLOW, fg = RED, width = 40, relief=RAISED, command = save)
 add_button.grid(row = 6, column = 1, columnspan=3)
 
 

e2061c77e93ec59ba7b6eaccf0bdfcd4e5e9b4d5
OmmDevgoswami <teamuniverse.omm@gmail.com>
2024-09-17 17:53:14 +0530

    NATO file updated !!

--- NATO-alphabet-start/main.py
+++ NATO-alphabet-start/main.py
@@ -1,8 +1,14 @@
 import pandas
 data = pandas.read_csv("NATO-alphabet-start\\nato_phonetic_alphabet.csv")
 nato_phonetic = {row.letter:row.code for (index,row) in data.iterrows()}
-word = input("Enter the Word: ").upper()
-word_list = [letter for letter in word]
 
-NATO_expression = [nato_phonetic[NATO] if NATO in nato_phonetic.keys() else NATO for NATO in word_list ]
-print (NATO_expression)
+flag = True
+while(flag):
+    word = input("Enter the Word: ").upper()
+    if word.isalpha():
+        word_list = [letter for letter in word]
+        NATO_expression = [nato_phonetic[NATO] if NATO in nato_phonetic.keys() else NATO for NATO in word_list ]
+        print (NATO_expression)
+        flag = False
+    else:
+        print("Invalid input. Please enter a word.")

9afb15b93d82ef3977b5276d4173cac1edbf5436
OmmDevgoswami <teamuniverse.omm@gmail.com>
2025-02-22 17:03:57 +0530

    Completed the Nope Swap Task

--- Tinder_Nope_Bot.py
+++ Tinder_Nope_Bot.py
@@ -1,51 +1,58 @@
-"""Needs More Work"""
+"""
+Login need to be done manually as their is a captcha adn opt authentication
 
-# from selenium import webdriver
-# from selenium.webdriver.common.by import By
-# from selenium.webdriver.common.keys import Keys
-# from selenium.common.exceptions import NoSuchElementException
-# import time
+AS Tinder hasa strong bot detetction, it changes element from time to time
+"""
 
-# chrome_option = webdriver.ChromeOptions()
-# chrome_option.add_experimental_option("detach", True)
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import NoSuchElementException
+import time
 
-# driver = webdriver.Chrome(options = chrome_option)
-# driver.get("https://tinder.com/")
-# driver.maximize_window()
+chrome_option = webdriver.ChromeOptions()
+chrome_option.add_experimental_option("detach", True)
 
-# logIn = driver.find_element(By.XPATH, '//*[@id="o-1274314283"]/div/div[1]/div/main/div[1]/div/div/div/div/div/header/div/div[2]/div[2]/a/div[2]/div[2]')
-# logIn.click()
-# time.sleep(2)
-# try:
-#     phoneLogin = driver.find_element(By.XPATH, '//*[@id="o-1052973847"]/div/div[1]/div/div[1]/div/div/div[2]/div[2]/span/div[3]/button/div[2]/div[2]')
-#     phoneLogin.click()
-#     time.sleep(2)
-#     phoneNumber = driver.find_element(By.NAME, "phone_number")
-#     phoneNumber.send_keys("8249100154", Keys.ENTER)
-#     time.sleep(60)#Time for Captcha and OTP Verification
-# except NoSuchElementException as e:
-#     moreOption = driver.find_element(By.XPATH, '//*[@id="o-1052973847"]/div/div[1]/div/div[1]/div/div/div[2]/div[2]/span/button')
-#     moreOption.click()
-#     time.sleep(2)
-#     phoneLogin = driver.find_element(By.XPATH, '//*[@id="o-1052973847"]/div/div[1]/div/div[1]/div/div/div[2]/div[2]/span/div[3]/button/div[2]/div[2]')
-#     phoneLogin.click()
-#     time.sleep(2)
-#     phoneNumber = driver.find_element(By.NAME, "phone_number")
-#     phoneNumber.send_keys("8249100154", Keys.ENTER)
-#     time.sleep(60)
+driver = webdriver.Chrome(options = chrome_option)
+driver.get("https://tinder.com/")
+driver.maximize_window()
 
-# allow = driver.find_element(By.XPATH, '//*[@id="o-1052973847"]/div/div[2]/div/div/div[1]/div[1]/button/div[2]/div[2]')
-# allow.click()
-# print("I am okay")
-# allowButton = driver.find_element(By.XPATH, '//*[@id="o-1052973847"]/div/div[1]/div/div/div[3]/button[1]/div[2]')
-# allowButton.click()
-# time.sleep(2)
-# notificationButton = driver.find_element(By.XPATH, '//*[@id="o-1052973847"]/div/div[1]/div/div/div[3]/button[2]/div[2]')
-# notificationButton.click()
-# print("I passed")
+logIn = driver.find_element(By.XPATH, '//*[@id="o-1274314283"]/div/div[1]/div/main/div[1]/div/div/div/div/div/header/div/div[2]/div[2]/a/div[2]')
+logIn.click()
+time.sleep(2)
+try:
+    phoneLogin = driver.find_element(By.XPATH, '//*[@id="o-1052973847"]/div/div[1]/div/div[1]/div/div/div[2]/div[2]/span/div[3]/button/div[2]/div[2]')
+    phoneLogin.click()
+    time.sleep(2)
+    phoneNumber = driver.find_element(By.NAME, "phone_number")
+    phoneNumber.send_keys("8249100154", Keys.ENTER)
+    time.sleep(90)#Time for Captcha and OTP Verification
+except NoSuchElementException as e:
+    moreOption = driver.find_element(By.XPATH, '//*[@id="o-1052973847"]/div/div[1]/div/div[1]/div/div/div[2]/div[2]/span/button')
+    moreOption.click()
+    time.sleep(2)
+    phoneLogin = driver.find_element(By.XPATH, '//*[@id="o-1052973847"]/div/div[1]/div/div[1]/div/div/div[2]/div[2]/span/div[3]/button/div[2]/div[2]')
+    phoneLogin.click()
+    time.sleep(2)
+    phoneNumber = driver.find_element(By.NAME, "phone_number")
+    phoneNumber.send_keys("8249100154", Keys.ENTER)
+    time.sleep(90)
 
-# for _ in range(10):
-#     time.sleep(15)
-#     nope = driver.find_element(By.XPATH, '//button[contains(@aria-label, "Nope")]')
-#     nope.click()
-#     print("Nope Done")
+allow = driver.find_element(By.XPATH, '//*[@id="o-1052973847"]/div/div[2]/div/div/div[1]/div[1]/button/div[2]/div[2]')
+allow.click()
+print("I am okay")
+allowButton = driver.find_element(By.XPATH, '//*[@id="o-1052973847"]/div/div[1]/div/div/div[3]/button[1]/div[2]')
+allowButton.click()
+time.sleep(2)
+notificationButton = driver.find_element(By.XPATH, '//*[@id="o-1052973847"]/div/div[1]/div/div/div[3]/button[2]/div[2]')
+notificationButton.click()
+print("I passed")
+
+body = driver.find_element(By.TAG_NAME, "body")
+
+for _ in range(10):
+    time.sleep(30)
+    # nope = driver.find_element(By.XPATH, '//button[contains(@aria-label, "Nope")]')
+    body.send_keys(Keys.ARROW_LEFT)
+    print("Nope Done")
+    

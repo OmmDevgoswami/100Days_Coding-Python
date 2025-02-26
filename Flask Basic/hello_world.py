84be1404d6bff938f3812466f80b192ce9138870
OmmDevgoswami <teamuniverse.omm@gmail.com>
2025-02-26 15:30:22 +0530

    Flask basic - 2

--- Flask Basic/hello_world.py
+++ Flask Basic/hello_world.py
@@ -2,6 +2,13 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/")
-def hellow_world():
+@app.route("/")         #/ incideates to the Home page
+def hello_world():
     return "<p> Hello World </p>"
+
+@app.route("/bye")      #/bye is another page
+def bye_world():
+    return "<p> Bye World </p>"
+
+if __name__ == "__main__":
+    app.run()

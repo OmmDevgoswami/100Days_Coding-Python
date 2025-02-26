bddf4f0f2dbb71ff28093681b95ad46f4a72cb4e
OmmDevgoswami <teamuniverse.omm@gmail.com>
2025-02-26 19:13:00 +0530

    Decorater with Flask

--- Flask Basic/hello_world.py
+++ Flask Basic/hello_world.py
@@ -1,5 +1,24 @@
 from flask import Flask
 
+def makeBold(elements):
+    def wrapper():
+        element = elements()
+        return f"<b>{element}</b>"
+    return wrapper
+
+def makeUnderline(elements):
+    def wrapper():
+        element = elements()
+        return f"<u>{element}</u>"
+    return wrapper
+
+def makeItalic(elements):
+    def wrapper():
+        element = elements()
+        return f"<i>{element}</i>"
+    return wrapper
+
+
 app = Flask(__name__)
 
 @app.route("/")         #/ incideates to the Home page
@@ -7,6 +26,9 @@ def hello_world():
     return "<p> Hello World </p>"
 
 @app.route("/bye")      #/bye is another page
+@makeBold
+@makeItalic
+@makeUnderline
 def bye_world():
     return "<p> Bye World </p>"
 

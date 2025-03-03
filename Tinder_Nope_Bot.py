1cc2b2bec3755bc72ca3a1f96cd156c8c9d144e0
OmmDevgoswami <teamuniverse.omm@gmail.com>
2025-03-03 23:54:40 +0530

    Path Updated

--- Tinder_Nope_Bot.py
+++ Tinder_Nope_Bot.py
@@ -17,7 +17,7 @@ driver = webdriver.Chrome(options = chrome_option)
 driver.get("https://tinder.com/")
 driver.maximize_window()
 
-logIn = driver.find_element(By.XPATH, '//*[@id="o-1274314283"]/div/div[1]/div/main/div[1]/div/div/div/div/div/header/div/div[2]/div[2]/a/div[2]')
+logIn = driver.find_element(By.XPATH, '//*[@id="s187265716"]/div/div[1]/div/main/div[1]/div/div/div/div/div/header/div/div[2]/div[2]/a/div[2]')
 logIn.click()
 time.sleep(2)
 try:
@@ -28,23 +28,23 @@ try:
     phoneNumber.send_keys("8249100154", Keys.ENTER)
     time.sleep(90)#Time for Captcha and OTP Verification
 except NoSuchElementException as e:
-    moreOption = driver.find_element(By.XPATH, '//*[@id="o-1052973847"]/div/div[1]/div/div[1]/div/div/div[2]/div[2]/span/button')
+    moreOption = driver.find_element(By.XPATH, '//*[@id="s408606152"]/div/div[1]/div/div[1]/div/div/div[2]/div[2]/span/button')
     moreOption.click()
     time.sleep(2)
-    phoneLogin = driver.find_element(By.XPATH, '//*[@id="o-1052973847"]/div/div[1]/div/div[1]/div/div/div[2]/div[2]/span/div[3]/button/div[2]/div[2]')
+    phoneLogin = driver.find_element(By.XPATH, '//*[@id="s408606152"]/div/div[1]/div/div[1]/div/div/div[2]/div[2]/span/div[3]/button/div[2]/div[2]')
     phoneLogin.click()
     time.sleep(2)
     phoneNumber = driver.find_element(By.NAME, "phone_number")
     phoneNumber.send_keys("8249100154", Keys.ENTER)
     time.sleep(90)
 
-allow = driver.find_element(By.XPATH, '//*[@id="o-1052973847"]/div/div[2]/div/div/div[1]/div[1]/button/div[2]/div[2]')
+allow = driver.find_element(By.XPATH, '//*[@id="s408606152"]/div/div[2]/div/div/div[1]/div[1]/button/div[2]/div[2]')
 allow.click()
 print("I am okay")
-allowButton = driver.find_element(By.XPATH, '//*[@id="o-1052973847"]/div/div[1]/div/div/div[3]/button[1]/div[2]')
+allowButton = driver.find_element(By.XPATH, '//*[@id="s408606152"]/div/div[1]/div/div/div[3]/button[1]/div[2]/div[2]')
 allowButton.click()
 time.sleep(2)
-notificationButton = driver.find_element(By.XPATH, '//*[@id="o-1052973847"]/div/div[1]/div/div/div[3]/button[2]/div[2]')
+notificationButton = driver.find_element(By.XPATH, '//*[@id="s408606152"]/div/div/div/div/div[3]/button[2]/div[2]/div[2]/div')
 notificationButton.click()
 print("I passed")
 

ddfa373eda55b6abe048ef2c34cd8f84fb93651f
OmmDevgoswami <teamuniverse.omm@gmail.com>
2025-03-05 23:15:27 +0530

    Jinja Basics

--- Jinja Basics/main.py
+++ Jinja Basics/main.py
@@ -1,10 +1,17 @@
 from flask import Flask, render_template
+import random
+import datetime as dt
 
 app = Flask(__name__)
 
+name = "Omm Devgoswami"
+randomNumber = random.random()
+year = dt.datetime.now()
+currentYear = year.year
+
 @app.route("/")
 def home():
-    return render_template("index.html")
+    return render_template("index.html", num=randomNumber, year = currentYear, name = name)
 
 if __name__ == "__main__":
     app.run(debug = True)

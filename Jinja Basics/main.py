ef90d12e91c3c8064c185bcc00236e65f8fd2f33
OmmDevgoswami <teamuniverse.omm@gmail.com>
2025-03-06 10:03:22 +0530

    Jinja Basics - 2

--- Jinja Basics/main.py
+++ Jinja Basics/main.py
@@ -1,10 +1,11 @@
 from flask import Flask, render_template
 import random
 import datetime as dt
+import requests
 
 app = Flask(__name__)
 
-name = "Omm Devgoswami"
+name = "Omm"
 randomNumber = random.random()
 year = dt.datetime.now()
 currentYear = year.year
@@ -13,5 +14,29 @@ currentYear = year.year
 def home():
     return render_template("index.html", num=randomNumber, year = currentYear, name = name)
 
+@app.route("/guess/<number>")
+def guess_page(number):
+    def genderFinder():
+            gender = requests.get("https://api.genderize.io", params = {"name": name})
+            gender.raise_for_status()
+            genderGuess = gender.json()
+            return genderGuess["gender"]
+    def ageFinder():
+            age = requests.get("https://api.agify.io", params = {"name": name})
+            age.raise_for_status()
+            ageGuess = age.json()
+            return ageGuess["age"]
+        
+    gender = genderFinder()
+    age = ageFinder()
+    return render_template("guess.html", user_name = name, gender = gender, age = age, num = number)
+
+@app.route("/blog")
+def blog_page():
+    response = requests.get(" https://api.npoint.io/c790b4d5cab58020d391")
+    response.raise_for_status()
+    allPosts = response.json()
+    return render_template("fake_blog.html", posts = allPosts)
+    
 if __name__ == "__main__":
     app.run(debug = True)

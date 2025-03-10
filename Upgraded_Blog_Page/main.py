46a0b90488c557e2c3ef69f0b015439f4b919cc2
OmmDevgoswami <teamuniverse.omm@gmail.com>
2025-03-10 20:53:43 +0530

    Updated Fake Blog

---
+++ Upgraded_Blog_Page/main.py
@@ -0,0 +1,44 @@
+from flask import Flask, render_template
+import requests
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    response = requests.get("https://api.npoint.io/c790b4d5cab58020d391")
+    response.raise_for_status()
+    data = response.json()
+    
+    return render_template("index.html", posts = data)
+
+# @app.route("/about.html")
+# def about():
+#     return render_template("about.html")
+
+# @app.route("/contact.html")
+# def contact():
+#     return render_template("contact.html")
+
+#A more innovative Solution
+@app.route("/<filename>")
+def pages(filename):
+    response = requests.get("https://api.npoint.io/c790b4d5cab58020d391")
+    response.raise_for_status()
+    data = response.json()
+    return render_template(filename, posts = data)
+
+@app.route("/post/<postNo>")
+def post_page(postNo):
+    response = requests.get("https://api.npoint.io/5808b1a3304a496f4c65")
+    response.raise_for_status()
+    data = response.json()
+    
+    post_data = None
+    for _ in data:
+        if _["id"] == int(postNo):
+            post_data = _
+            break
+    return render_template("post.html", post = post_data)
+
+if __name__ == "__main__":
+    app.run(debug=True)
